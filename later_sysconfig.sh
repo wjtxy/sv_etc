890b66cd5101ce64e790014a61257b503493aa31
vivy89 <sv@arch.localdomain>
2021-06-30 09:09:33 +0800

    version 3.2

--- later_sysconfig.sh
+++ later_sysconfig.sh
@@ -13,8 +13,11 @@ sudo pacman -S --noconfirm lib32-gcc-libs the_silver_searcher bc lzop \
 		fcitx5 fcitx5-gtk fcitx5-qt fcitx5-material-color fcitx5-mozc \
 		fcitx5-configtool fcitx5-rime pulseaudio-alsa alsa-utils nfs-utils \
 		clang grim slurp llvm ccls
+yay -S --noconfirm google-chrome 
 
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions /home/sv/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting /home/sv/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
 cp /home/sv/sv_etc/.zshrc /home/sv/ -rvf
 source /home/sv/.zshrc
+rehash
