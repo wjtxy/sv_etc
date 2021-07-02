f671ecac53f9210d9a40142250e456d5632cf861
vivy89 <sv@arch.localdomain>
2021-07-03 02:28:49 +0800

    alacritty

--- script/alacritty.sh
+++ script/alacritty.sh
@@ -6,5 +6,6 @@
 #########################################################################
 #!/bin/bash
 
-# env LIBGL_ALWAYS_SOFTWARE=1 alacritty
-env WINIT_UNIX_BACKEND=x11 LIBGL_ALWAYS_SOFTWARE=1 alacritty
+# env LIBGL_ALWAYS_SOFTWARE=1 # virtualbox need,bug:不透明
+# env WINIT_UNIX_BACKEND=x11 LIBGL_ALWAYS_SOFTWARE=1 alacritty
+env WINIT_UNIX_BACKEND=x11 alacritty
